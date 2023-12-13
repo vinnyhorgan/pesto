fd7ef1c1c18a522b4fa075c422f2de0c2b0d58ea
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2023-12-13 21:20:36 +0100

    Worked on project manager

--- src/scripts/manager.lua
+++ src/scripts/manager.lua
@@ -1,23 +1,56 @@
 local icon, github, love, lua, raylib = pesto.graphics.loadLogos()
 icon:setFilter("bilinear")
 
+local major, minor, patch, codename = pesto.getVersion()
+
 if not pesto.filesystem.exists("projects") then
     pesto.filesystem.createDirectory("projects")
 end
 
 local projects = {}
 
-local items = pesto.filesystem.getDirectoryItems("projects")
-
-for _, item in pairs(items) do
-    if pesto.filesystem.isDirectory(item) then
-        if pesto.filesystem.exists(item .. "/main.lua") then
-            table.insert(projects, pesto.filesystem.getFilename(item))
-        end
+for _, item in pairs(pesto.filesystem.getDirectoryItems("projects")) do
+    if pesto.filesystem.isDirectory(item) and pesto.filesystem.exists(item .. "/main.lua") then
+        table.insert(projects, pesto.filesystem.getFilename(item))
     end
 end
 
-local currentPage = "projectList"
+local function wave(lo, hi, t)
+    local value = (math.sin(t) + 1) / 2
+    return lo + (hi - lo) * value
+end
+
+local Letter = pesto.Object:extend()
+
+function Letter:new(x, y, letter, index)
+    self.x = x
+    self.y = y
+    self.letter = letter
+    self.index = index
+    self.currentY = y
+    self.color = { 0, 0, 0 }
+end
+
+function Letter:update(dt)
+    self.currentY = wave(self.y - 6, self.y + 6, pesto.system.getTime() * 4 + self.index * 0.5)
+
+    self.color[1], self.color[2], self.color[3] = pesto.graphics.hsvToRgb(
+        (pesto.system.getTime() * 72 + self.index * 36) % 360, 0.8, 0.8)
+end
+
+function Letter:draw()
+    pesto.graphics.setColor(self.color[1], self.color[2], self.color[3])
+    pesto.graphics.textBig(self.letter, self.x, self.currentY)
+    pesto.graphics.setColor(255, 255, 255)
+end
+
+local letters = {}
+
+local function animatedText(text, x, y)
+    for i = 1, #text do
+        table.insert(letters, Letter(x + (i - 1) * 25, y, text:sub(i, i), i))
+    end
+end
 
 local Leaf = pesto.Object:extend()
 
@@ -25,14 +58,12 @@ function Leaf:new(x, y, direction)
     self.x = x
     self.y = y
     self.rotation = pesto.math.random(0, 360)
-    self.speed = pesto.math.random(100, 300) * direction
+    self.speed = pesto.math.random(100, 400) * direction
 end
 
 function Leaf:update(dt)
     self.x = self.x + self.speed * dt
-
     self.y = self.y + self.speed * dt * math.sin(self.rotation * math.pi / 180)
-
     self.rotation = self.rotation + self.speed * dt
 end
 
@@ -42,198 +73,198 @@ end
 
 local leaves = {}
 
-local letters = {}
-
-local function wave(lo, hi, t)
-    local value = (math.sin(t) + 1) / 2
-    return lo + (hi - lo) * value
-end
-
-local AnimatedLetter = pesto.Object:extend()
-
-function AnimatedLetter:new(x, y, letter, index)
-    self.x = x
-    self.y = y
-    self.letter = letter
-    self.index = index
-    self.currentY = y
-    self.color = { 0, 0, 0 }
-end
-
-function AnimatedLetter:update(dt)
-    self.color[1], self.color[2], self.color[3] = pesto.graphics.hsvToRgb(
-        (pesto.system.getTime() * 72 + self.index * 36) % 360, 0.8,
-        0.8)
-
-    self.currentY = wave(self.y - 6, self.y + 6, pesto.system.getTime() * 4 + self.index * 0.5)
-end
-
-function AnimatedLetter:draw()
-    pesto.graphics.setColor(self.color[1], self.color[2], self.color[3])
-
-    pesto.graphics.textBig(self.letter, self.x, self.currentY)
-end
-
-local function animatedText(text, x, y)
-    for i = 1, #text do
-        table.insert(letters, AnimatedLetter(x + (i - 1) * 25, y, text:sub(i, i), i))
-    end
-end
-
 function pesto.update(dt)
-    for _, leaf in pairs(leaves) do
-        leaf:update(dt)
-    end
-
     for _, letter in pairs(letters) do
         letter:update(dt)
     end
+
+    for _, leaf in pairs(leaves) do
+        leaf:update(dt)
+
+        if leaf.x > 960 + 100 or leaf.x < -100 then
+            pesto.util.remove(leaves, leaf)
+        end
+    end
 end
 
+local currentPage = "projectList"
+local scroll = 0
+
 function pesto.draw()
+    -- Sidebar
     pesto.graphics.setColor(50, 79, 59)
-    pesto.graphics.rectangle(0, 0, 250, 600)
+    pesto.graphics.rectangle(0, 0, 250, 540)
     pesto.graphics.setColor(255, 255, 255)
 
-    local major, minor, patch, codename = pesto.getVersion()
-
-    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 250 / 2 - icon:getWidth() / 2, 30, icon:getWidth(), icon:getHeight()) then
+    -- Icon
+    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 250 / 2 - icon:getWidth() / 2, 40, icon:getWidth(), icon:getHeight()) then
         if pesto.mouse.isPressed(0) then
             for i = 1, 50 do
                 table.insert(leaves, Leaf(-50, pesto.math.random(0, 540), 1))
-            end
-
-            for i = 1, 50 do
                 table.insert(leaves, Leaf(960 + 50, pesto.math.random(0, 540), -1))
             end
 
-            icon:draw(250 / 2 - icon:getWidth() / 2, 30)
+            icon:draw(250 / 2 - icon:getWidth() / 2, 40)
+        elseif pesto.mouse.isDown(0) then
+            icon:draw(250 / 2 - icon:getWidth() / 2, 40)
         else
-            icon:draw(250 / 2 - icon:getWidth() / 2 - 2, 30 - 2, 0, 1.2, 1.2)
+            icon:draw(250 / 2 - icon:getWidth() / 2 - 2, 40 - 2, 0, 1.2, 1.2)
         end
     else
-        icon:draw(250 / 2 - icon:getWidth() / 2, 30)
+        icon:draw(250 / 2 - icon:getWidth() / 2, 40)
     end
 
-    pesto.graphics.text("v" .. major .. "." .. minor .. "." .. patch .. " " .. codename, 20, 500)
-
-    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 100 - 6, 210, 40) and pesto.mouse.isPressed(0) then
+    -- Tabs
+    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 150 - 6, 210, 40) and pesto.mouse.isPressed(0) then
         currentPage = "projectList"
         letters = {}
     end
 
-    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 150 - 6, 210, 40) and pesto.mouse.isPressed(0) then
-        currentPage = "newProject"
-        letters = {}
-    end
-
-    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 200 - 6, 210, 40) and pesto.mouse.isPressed(0) then
-        currentPage = "templates"
-        letters = {}
-    end
-
-    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 250 - 6, 210, 40) and pesto.mouse.isPressed(0) then
-        currentPage = "about"
-        animatedText("Pesto " .. major .. "." .. minor .. "." .. patch .. " " .. codename, 290, 150)
-    end
-
     if currentPage == "projectList" then
-        pesto.graphics.setColor(37, 59, 44)
-        pesto.graphics.rectangleRounded(20, 100 - 6, 210, 40, 0.2, 10)
-        pesto.graphics.setColor(255, 255, 255)
-    end
-
-    local width = pesto.graphics.measureMedium("Project List")
-    pesto.graphics.textMedium("Project List", 125 - width / 2, 100)
-
-    if currentPage == "newProject" then
         pesto.graphics.setColor(37, 59, 44)
         pesto.graphics.rectangleRounded(20, 150 - 6, 210, 40, 0.2, 10)
         pesto.graphics.setColor(255, 255, 255)
     end
 
-    width = pesto.graphics.measureMedium("New Project")
-    pesto.graphics.textMedium("New Project", 125 - width / 2, 150)
+    local width = pesto.graphics.measureMedium("Project List")
+    pesto.graphics.textMedium("Project List", 250 / 2 - width / 2, 150)
 
-    if currentPage == "templates" then
+    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 200 - 6, 210, 40) and pesto.mouse.isPressed(0) then
+        currentPage = "newProject"
+        letters = {}
+    end
+
+    if currentPage == "newProject" then
         pesto.graphics.setColor(37, 59, 44)
         pesto.graphics.rectangleRounded(20, 200 - 6, 210, 40, 0.2, 10)
         pesto.graphics.setColor(255, 255, 255)
     end
 
-    width = pesto.graphics.measureMedium("Templates")
-    pesto.graphics.textMedium("Templates", 125 - width / 2, 200)
+    width = pesto.graphics.measureMedium("New Project")
+    pesto.graphics.textMedium("New Project", 250 / 2 - width / 2, 200)
 
-    if currentPage == "about" then
+    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 250 - 6, 210, 40) and pesto.mouse.isPressed(0) then
+        currentPage = "templates"
+        letters = {}
+    end
+
+    if currentPage == "templates" then
         pesto.graphics.setColor(37, 59, 44)
         pesto.graphics.rectangleRounded(20, 250 - 6, 210, 40, 0.2, 10)
         pesto.graphics.setColor(255, 255, 255)
     end
 
-    width = pesto.graphics.measureMedium("About")
-    pesto.graphics.textMedium("About", 125 - width / 2, 250)
+    width = pesto.graphics.measureMedium("Templates")
+    pesto.graphics.textMedium("Templates", 250 / 2 - width / 2, 250)
 
+    if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 20, 300 - 6, 210, 40) and pesto.mouse.isPressed(0) then
+        currentPage = "about"
+        animatedText("Pesto " .. major .. "." .. minor .. "." .. patch .. " " .. codename, 280, 150)
+    end
+
+    if currentPage == "about" then
+        pesto.graphics.setColor(37, 59, 44)
+        pesto.graphics.rectangleRounded(20, 300 - 6, 210, 40, 0.2, 10)
+        pesto.graphics.setColor(255, 255, 255)
+    end
+
+    width = pesto.graphics.measureMedium("About")
+    pesto.graphics.textMedium("About", 250 / 2 - width / 2, 300)
+
+    -- Version
+    width = pesto.graphics.measure("v" .. major .. "." .. minor .. "." .. patch .. " " .. codename)
+    pesto.graphics.text("v" .. major .. "." .. minor .. "." .. patch .. " " .. codename, 250 / 2 - width / 2,
+        540 - 40 - 18)
+
+    -- Main
     pesto.graphics.clear(31, 49, 47)
 
     if currentPage == "projectList" then
         pesto.graphics.setColor(50, 79, 59)
-        pesto.graphics.rectangleRounded(290, 150, 630, 350, 0.05, 10)
+        pesto.graphics.rectangleRounded(290, 150, 960 - 250 - 80, 540 - 150 - 40, 0.05, 10)
         pesto.graphics.setColor(255, 255, 255)
 
         pesto.graphics.textBig("Project List", 290, 40)
 
-        for i, project in ipairs(projects) do
-            if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 290, 120 + 70 * i - 15, 630, 60) then
+        local _, wheelY = pesto.mouse.getWheelMove()
+
+        if wheelY > 0 then
+            scroll = scroll + 20
+        elseif wheelY < 0 then
+            scroll = scroll - 20
+        end
+
+        pesto.graphics.beginScissorMode(290, 190, 960 - 250 - 80, 540 - 150 - 40 - 80)
+
+        for i, project in pairs(projects) do
+            if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 290 + 40, scroll + 150 + 60 * i - 10, 550, 50) then
                 pesto.graphics.setColor(37, 59, 44)
-                pesto.graphics.rectangleRounded(330, 120 + 70 * i - 15, 550, 60, 0.05, 10)
+                pesto.graphics.rectangleRounded(290 + 40, scroll + 150 + 60 * i - 10, 550, 50, 0.1, 10)
                 pesto.graphics.setColor(255, 255, 255)
             end
 
             width = pesto.graphics.measureMedium(project)
-            pesto.graphics.textMedium(project, 290 + 630 / 2 - width / 2, 120 + 70 * i)
+            pesto.graphics.textMedium(project, 290 + 630 / 2 - width / 2, scroll + 150 + 60 * i)
 
-            pesto.graphics.line(330, 120 + 70 * i - 15 + 60, 880, 120 + 70 * i - 15 + 60)
+            pesto.graphics.line(290 + 40, scroll + 150 + 60 * i - 15 + 60, 880, scroll + 150 + 60 * i - 15 + 60)
 
-            if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 290, 100 + 70 * i - 6, 470, 60) and pesto.mouse.isPressed(0) then
-                package.path = "projects/" .. project .. "/?.lua;" .. package.path
-                package.loaded.main = nil
-                pesto.filesystem.changeDirectory("projects/" .. project)
-                require("main")
+            if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 290 + 40, scroll + 150 + 60 * i - 10, 550, 50) and pesto.mouse.isPressed(0) then
+                --package.path = "projects/" .. project .. "/?.lua;" .. package.path
+                --package.loaded.main = nil
+                --pesto.filesystem.changeDirectory("projects/" .. project)
+                --require("main")
             end
 
-            if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 290, 100 + 70 * i - 6, 470, 60) and pesto.mouse.isPressed(1) then
-                os.execute("explorer " .. pesto.filesystem.getWorkingDirectory() .. "\\projects\\" .. project)
+            if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 290 + 40, scroll + 150 + 60 * i - 10, 550, 50) and pesto.mouse.isPressed(1) then
+                if pesto.system.getOS() == "windows" then
+                    os.execute("explorer " .. pesto.filesystem.getWorkingDirectory() .. "\\projects\\" .. project)
+                end
             end
         end
+
+        pesto.graphics.endScissorMode()
     elseif currentPage == "newProject" then
         pesto.graphics.textBig("New Project", 290, 40)
     elseif currentPage == "templates" then
         pesto.graphics.textBig("Templates", 290, 40)
+        pesto.graphics.textBig("Coming Soon", 290, 150)
     elseif currentPage == "about" then
         pesto.graphics.textBig("About", 290, 40)
 
         width = pesto.graphics.measureMedium("By Vinny Horgan with love <3")
-        pesto.graphics.textMedium("By Vinny Horgan with love <3", 290 + 630 / 2 - width / 2, 250)
+        pesto.graphics.textMedium("By Vinny Horgan with love <3", 250 + 710 / 2 - width / 2, 250)
 
-        github:draw(290 + 630 / 2 - github:getWidth() / 2, 300)
-
-        if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 290 + 630 / 2 - github:getWidth() / 2, 300, github:getWidth(), github:getHeight()) and pesto.mouse.isPressed(0) then
+        if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 250 + 710 / 2 - github:getWidth() / 2, 300, github:getWidth(), github:getHeight()) and pesto.mouse.isPressed(0) then
             pesto.system.openURL("https://github.com/vinnyhorgan/pesto")
         end
 
-        width = pesto.graphics.measureMedium("Also thanks to these amazing people:")
-        pesto.graphics.textMedium("Also thanks to these amazing people:", 290 + 630 / 2 - width / 2, 370)
+        github:draw(250 + 710 / 2 - github:getWidth() / 2, 300)
 
-        love:draw(290, 420)
-        lua:draw(290 + 100, 420)
-        raylib:draw(290 + 200, 420)
+        width = pesto.graphics.measureMedium("Also thanks to these amazing people")
+        pesto.graphics.textMedium("Also thanks to these amazing people", 290 + 630 / 2 - width / 2, 360)
+
+        if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 250 + 710 / 2 - lua:getWidth() / 2, 430, lua:getWidth(), lua:getHeight()) and pesto.mouse.isPressed(0) then
+            pesto.system.openURL("https://www.lua.org/")
+        end
+
+        lua:draw(250 + 710 / 2 - lua:getWidth() / 2, 430)
+
+        if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 250 + 710 / 2 - love:getWidth() / 2 - 100, 430, love:getWidth(), love:getHeight()) and pesto.mouse.isPressed(0) then
+            pesto.system.openURL("https://love2d.org/")
+        end
+
+        love:draw(250 + 710 / 2 - lua:getWidth() / 2 - 100, 430)
+
+        if pesto.graphics.checkCollisionPointRec(pesto.mouse.getX(), pesto.mouse.getY(), 250 + 710 / 2 - raylib:getWidth() / 2 + 100, 430, raylib:getWidth(), raylib:getHeight()) and pesto.mouse.isPressed(0) then
+            pesto.system.openURL("https://www.raylib.com/")
+        end
+
+        raylib:draw(250 + 710 / 2 - lua:getWidth() / 2 + 100, 430)
     end
 
     for _, letter in pairs(letters) do
         letter:draw()
     end
-
-    pesto.graphics.setColor(255, 255, 255)
 
     for _, leaf in pairs(leaves) do
         leaf:draw()
